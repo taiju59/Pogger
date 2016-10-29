3fd1d222058bd53b6c27967fe294028f81b691df
Taiju Aoki <taiju.dev@gmail.copm>
2016-10-29 20:57:46 +0900

    Pog一覧画面の処理をListViewControllerへ共通化

--- Pogger/ListViewController.swift
+++ Pogger/ListViewController.swift
@@ -7,18 +7,329 @@
 //
 
 import UIKit
+import RealmSwift
+import GoogleMaps
+import DZNEmptyDataSet
 
-class ListViewController: UIViewController {
+enum ListType {
+    case records
+    case favorites
+}
+
+class ListViewController: UIViewController, UITabBarControllerDelegate, UITableViewDataSource, UITableViewDelegate, DZNEmptyDataSetSource, PointCellDelegate {
+
+    var viaTableView: UITableView! {
+        return nil
+    }
+
+    var listType: ListType! {
+        return nil
+    }
+
+    private let dispMinuteMin = 10
+    private let comeBackLimit = 60
+    private let dispCellCnt = Int(INT_MAX) // TODO: 表示セル制限
+
+    private var pointsData: [[FixedPoint]]?
+    private var selectedPoint: FixedPoint?
+
+    private var token: NotificationToken?
+
+    var pointCellType: PointCellType = .streetView
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        viaTableView.tableFooterView = UIView()
+        viaTableView.emptyDataSetSource = self
+
+        self.token = try! Realm().addNotificationBlock { note, realm in
+            if self.pointsData == nil || self.pointsData!.isEmpty {
+                self.refreshData()
+            }
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tabBarController!.delegate = self
+        refreshData()
+    }
+
+    //MARK: - TableView
+    func numberOfSections(in tableView: UITableView) -> Int {
+
+        let dayCnt = pointsData?.count ?? 0
+        return dayCnt// > 5 ? 5:dayCnt
+    }
+
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        guard let pd = pointsData else {
+            return nil
+        }
+        if pd.isEmpty {
+            return nil
+        }
+        guard let date = pd[section][0].startDate else {
+            return nil
+        }
+        let days = Utils.getDayCnt(to: date)
+        var sectionTitle: String?
+        switch days {
+        case 0:
+            sectionTitle = "今日"
+        case 1:
+            sectionTitle = "昨日"
+        default:
+            sectionTitle = Utils.getDateString(for: date)
+        }
+        return sectionTitle
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pointsData?[section].count ?? 0
     }
 
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let section = (indexPath as NSIndexPath).section
+        let row = (indexPath as NSIndexPath).row
+        let point = pointsData![section][row]
+
+        let cell: PointCell = tableView.dequeueReusableCell(withIdentifier: "PointCell") as! PointCell
+        cell.delegate = self
+        cell.setPoint(point, dispMinuteMin: dispMinuteMin, type: self.pointCellType)
+
+        return cell
+    }
+
+    func refreshData() {
+        let private_queue = DispatchQueue(label: "refreshData", attributes: [])
+        private_queue.async {
+            defer {
+                DispatchQueue.main.async {
+                    self.viaTableView.reloadData()
+                }
+            }
+            self.pointsData = []
+            switch self.listType! {
+            case .records:
+                self.setPogList()
+            case .favorites:
+                self.setFavoriteList()
+            }
+        }
+    }
+
+    private func setPogList() {
+        let allPoints = try! Realm().objects(Point.self).sorted(byProperty: "startDate", ascending: false)
+        if allPoints.isEmpty {
+            return
+        }
+        let lastPointDate = allPoints[0].startDate!
+
+        let predicate = NSPredicate(format: "stayMin >= %d OR startDate = %@", self.dispMinuteMin, lastPointDate as CVarArg)
+        let points = allPoints.filter(predicate)
+
+        for point in points {
+            let fPoint = FixedPoint(rlm: point)
+            // 1個目
+            guard let lastDayPoints = self.pointsData!.last else {
+                self.pointsData!.append([fPoint])
+                continue
+            }
+            guard let lastPoint = lastDayPoints.last else {
+                // 通らないはず
+                continue
+            }
+            if fPoint.startDate!.isInSameDayAsDate(lastPoint.startDate!) {
+                // 同じ日
+                self.pointsData![self.pointsData!.count - 1].append(fPoint)
+            } else {
+                // 違う日
+                self.pointsData!.append([fPoint])
+            }
+            if self.pointsData.flatMap({$0})!.count >= self.dispCellCnt {
+                // 表示限界越え
+                break
+            }
+        }
+    }
+
+    private func setFavoriteList() {
+        let predicate = NSPredicate(format: "favorite = true")
+        let points = try! Realm().objects(Point.self).sorted(byProperty: "startDate", ascending: false).filter(predicate)
+        if points.isEmpty {
+            return
+        }
+
+        for point in points {
+            let fPoint = FixedPoint(rlm: point)
+            // 1個目
+            guard let lastDayPoints = self.pointsData!.last else {
+                self.pointsData!.append([fPoint])
+                continue
+            }
+            guard let lastPoint = lastDayPoints.last else {
+                // 通らないはず
+                continue
+            }
+            if fPoint.startDate!.isInSameDayAsDate(lastPoint.startDate!) {
+                // 同じ日
+                self.pointsData![self.pointsData!.count - 1].append(fPoint)
+            } else {
+                // 違う日
+                self.pointsData!.append([fPoint])
+            }
+            if self.pointsData.flatMap({$0})!.count >= self.dispCellCnt {
+                // 表示限界超え
+                break
+            }
+        }
+    }
+
+    //MARK: - Action
+    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        if let points = pointsData {
+            selectedPoint = points[(indexPath as NSIndexPath).section][(indexPath as NSIndexPath).row]
+        }
+        return indexPath
+    }
+
+    func pointCell(_ pointCell: PointCell, didTapFavButton select: Bool) {
+        Point.switchFavorite(pointCell.id, select: select)
+    }
+
+    func didTapShareButton(_ pointCell: PointCell) {
+        let point = try! Realm().objects(Point.self).filter("id == \"\(pointCell.id!)\"")[0]
+        let name = point.name ?? ""
+        let locality = point.locality ?? ""
+
+        let shareText = "\(name),\(locality)"
+        let activityItems = [shareText]
+        //TODO: 「コピー」ではなく「住所をコピー」にする
+        //TODO: 「住所をコピー」完了時に「✅」を表示
+        //TODO: 「マップで開く」を追加
+        let activityVC = UIActivityViewController(activityItems: activityItems, applicationActivities: nil)
+
+        present(activityVC, animated: true, completion: nil)
+    }
+
+    func didTapOptionButton(_ pointCell: PointCell) {
+        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        actionSheet.view.tintColor = Prefix.themaColor
+        //TODO: 「この場所を今後無視する」のアクションを追加
+        let deleteAction = UIAlertAction(title: "この記録を削除", style: .destructive, handler: {
+            action in self.deleteRecord(pointCell)
+        })
+        let cancelAction = UIAlertAction(title: "キャンセル", style: .cancel, handler: nil)
+        actionSheet.addAction(deleteAction)
+        actionSheet.addAction(cancelAction)
+        self.present(actionSheet, animated: true, completion: nil)
+    }
+
+    func didLongSelect(_ sender: UILongPressGestureRecognizer) {
+        let point = sender.location(in: viaTableView)
+        let indexPath = viaTableView.indexPathForRow(at: point)!
+
+        if sender.state == .began {
+            let point = pointsData![(indexPath as NSIndexPath).section][(indexPath as NSIndexPath).row]
+
+            let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+            actionSheet.view.tintColor = Prefix.themaColor
+            let copyAction = UIAlertAction(title: "住所をコピー", style: .default, handler: {
+                action in self.copyAddress(point)
+            })
+            let mapOpenAction = UIAlertAction(title: "マップで見る", style: .default, handler: {
+                action in self.shouldOpenMap(point)
+            })
+            let cancelAction = UIAlertAction(title: "キャンセル", style: .cancel, handler: nil)
+            actionSheet.addAction(copyAction)
+            actionSheet.addAction(mapOpenAction)
+            actionSheet.addAction(cancelAction)
+            self.present(actionSheet, animated: true, completion: nil)
+        }
+    }
+
+    private func deleteRecord(_ pointCell: PointCell) {
+        // Realm から削除
+        let realm = try! Realm()
+        let point = realm.objects(Point.self).filter("id == \"\(pointCell.id!)\"")[0]
+        try! realm.write {
+            realm.delete(point)
+        }
+        let indexPath = viaTableView.indexPath(for: pointCell)!
+
+        // VC が保持しているデータから削除
+        pointsData![indexPath.section].remove(at: indexPath.row)
+
+        // tableView の見た目として削除
+        viaTableView.beginUpdates()
+        viaTableView.deleteRows(at: [indexPath], with: .automatic)
+        viaTableView.endUpdates()
+    }
+
+    private func copyAddress(_ point: FixedPoint) {
+        let name = point.name ?? ""
+        let locality = point.locality ?? ""
+        let address = "\(name),\(locality)"
+        let board = UIPasteboard.general
+        board.setValue(address, forPasteboardType: "public.text")
+    }
+
+    private func shouldOpenMap(_ point: FixedPoint) {
+        let ll = String(format: "%f,%f", point.latitude, point.longitude)
+        let name = point.name ?? ""
+        let locality = point.locality ?? ""
+        let q = "\(name),\(locality)"
+
+        let urlString: String
+        if UIApplication.shared.canOpenURL(URL(string: "comgooglemaps://")!) {
+            urlString = "comgooglemaps://?center=\(ll)&q=\(q)"
+        } else {
+            urlString = "http://maps.apple.com/?ll=\(ll)&q=\(q)"
+        }
+        let encodedUrl = urlString.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!
+        let url = URL(string: encodedUrl)!
+        UIApplication.shared.openURL(url)
+    }
+
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+        let index: Int!
+        switch listType! {
+        case .records:
+            index = 0
+        case .favorites:
+            index = 1
+        }
+        if tabBarController.selectedIndex == index {
+            scrollToTop()
+        }
+    }
+
+    func scrollToTop() {
+        viaTableView.setContentOffset(CGPoint(x: 0, y: -viaTableView.contentInset.top), animated: true)
+    }
+
+    //MARK : Transition
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        if segue.identifier == "toStreetView" {
+            let vc: StreetViewController = segue.destination as! StreetViewController
+            let coordinater = CLLocationCoordinate2D(latitude: selectedPoint!.latitude, longitude: selectedPoint!.longitude)
+            vc.coordinater = coordinater
+        }
+    }
+
+    //MARK: - DZNEmptyDataSet
+    func customView(forEmptyDataSet scrollView: UIScrollView!) -> UIView! {
+        let nibName: String!
+        switch listType! {
+        case .records:
+            nibName = "EmptyState"
+        case .favorites:
+            nibName = "FavoriteEmptyState"
+        }
+        let nib = UINib(nibName: nibName, bundle:nil)
+        return nib.instantiate(withOwner: nil, options: nil).first as! UIView
+    }
 }
